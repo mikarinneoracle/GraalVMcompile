eda5fa6a47f907135bf60406c37cde00886cb6ae
Oracle Public Cloud User <opc@dev.subnet11301059.vcn11301059.oraclevcn.com>
2021-12-16 20:34:58 +0000

    create

---
+++ src/main/java/com/oracle/graalvm/demo/hellospringbootapp/HelloSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.graalvm.demo.hellospringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/graalvm/demo/hellospringbootapp/HelloSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.graalvm.demo.hellospringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
